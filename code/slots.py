6dd3db16a3749cae850d09b06a1f7e8d783d3d71
Hiep Do Trong <dotronghiephd@gmail.com>
2024-04-08 11:16:10 +0700

    sort file

---
+++ code/slots.py
@@ -0,0 +1,8 @@
+class Slot:
+    def __init__(self, id, x, y, distance=0, car_parking_ID=None):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.distance = distance
+        self.car_parking_ID = car_parking_ID
+        
--- slots.py
+++
@@ -1,8 +0,0 @@
-class Slot:
-    def __init__(self, id, x, y, distance=0, car_parking_ID=None):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.distance = distance
-        self.car_parking_ID = car_parking_ID
-        
